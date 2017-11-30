d9cf150dee785c5fd75ad111326276a12996b0bf
Nitikan Crwx <nitikan@live.com>
2017-11-30 23:21:49 +0700

    hand Img

--- core/src/com/crwx/mygdx/game/GameScreen.java
+++ core/src/com/crwx/mygdx/game/GameScreen.java
@@ -8,11 +8,13 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen extends ScreenAdapter{
 	private BabySisterGame babysisterGame;
-	private Texture babysisterImg;
+	private Texture babyImg;
+	private Texture handImg;
 	 
 	public GameScreen(BabySisterGame babysisterGame) {
 	        this.babysisterGame = babysisterGame;
-	        babysisterImg = new Texture("baby.png");
+	        babyImg = new Texture("baby.png");
+	        handImg = new Texture("hand.png");
 	 }
 
 	@Override
@@ -21,7 +23,8 @@ public class GameScreen extends ScreenAdapter{
 	    Gdx.gl.glClearColor(210/255f, 239/255f, 255/255f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	    batch.begin();
-	    batch.draw(babysisterImg, 440, 350);
+	    batch.draw(babyImg, 440, 350);
+	    batch.draw(handImg, 200, 150);
 	    batch.end();
 	}
 }
